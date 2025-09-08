39856cea2c8cdd48b366f71968001fdb6d020528
RealGenius1 <sptho24@gmail.com>
2025-09-08 12:50:12 -0400

    comment about math

--- Scripts/Player/PlayerConroller.gd
+++ Scripts/Player/PlayerConroller.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const GRAVITY := 20.0
 const JUMP_FORCE := -1200.0   # negative = upward in Godot
 const MAX_HEIGHT := 36000
+# y = Random from a bit above player to about 32000
+# x = Random number from player to +- sqrt(3200^2 - x^2)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY
