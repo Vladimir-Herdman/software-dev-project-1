9a39605634ea06d51fda4c20554574e7f97f32fa
Vladimir Herdman <vovaherdman@gmail.com>
2025-09-08 20:47:55 -0400

    Created some more sound changes to connect to settings, just need death signal to connec death screen

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -5,25 +5,32 @@ var music_playing: bool = true
 
 func _ready() -> void:
 	hud_settings.connect("settings_closed", Callable(self, "settings_closed"))
+	hud_settings.connect("music_muted", Callable(self, "mute_music"))
+	hud_settings.connect("music_unmuted", Callable(self, "unmute_music"))
+	hud_settings.connect("deathsound_muted", Callable(self, "mute_deathsound"))
+	hud_settings.connect("deathsound_unmuted", Callable(self, "unmute_deathsound"))
 	$Music.play()
 
 func new_game() -> void:
-	if not music_playing:
+	if not music_playing and not GLOBAL.checkbox_music_mute:
 		$Music.play()
 		music_playing = true
 
 func restart_game() -> void:
 	$Music.stop()
 	await get_tree().create_timer(1.5).timeout
-	$Music.play()
+	if not GLOBAL.checkbox_music_mute:
+		$Music.play()
 
 #TODO: connect to 'hit' signal from character model
 func game_over() -> void:
-	$Music.stop(); music_playing = false
-	$DeathSound.play()
+	$Music.stop();
+	music_playing = false
+	if not GLOBAL.checkbox_deathsound_mute:
+		$DeathSound.play()
 
 func settings_open() -> void:
-	print(GLOBAL.checkbox_deathsound_mute)
+	pass
 
 func settings_closed() -> void:
 	pass
@@ -32,9 +39,24 @@ func pause_game() -> void:
 	$Music.stop(); music_playing = false
 
 func unpause_game() -> void:
-	$Music.play(); music_playing = true
+	if not GLOBAL.checkbox_music_mute:
+		$Music.play()
+		music_playing = true
 
 #TODO: connect to strawberry eaten signal to play
-func play_strawberry_sound():
+func play_strawberry_sound() -> void:
 	$Music.stop()
 	$EatStrawberrySound.play()
+
+func mute_music() -> void:
+	$Music.volume_db = -80
+
+func unmute_music() -> void:
+	$Music.volume_db = 0
+
+func mute_deathsound() -> void:
+	$DeathSound.volume_db = -80
+	$Hud/DeathVoice.volume_db = -80
+	
+func unmute_deathsound() -> void:
+	$DeathSound.volume_db = 0
--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 signal settings_closed
+signal music_muted
+signal music_unmuted
+signal deathsound_muted
+signal deathsound_unmuted
 
 const master_bus_index: int = 0
 
@@ -9,9 +13,13 @@ func _on_exit_button_pressed() -> void:
 
 func _on_music_checkbox_toggled(toggled_on: bool) -> void:
 	GLOBAL.checkbox_music_mute = toggled_on
+	if toggled_on: music_muted.emit()
+	else: music_unmuted.emit()
 
 func _on_death_sound_checkbox_toggled(toggled_on: bool) -> void:
 	GLOBAL.checkbox_deathsound_mute = toggled_on
+	if toggled_on: deathsound_muted.emit()
+	else: deathsound_unmuted.emit()
 
 func _on_master_volume_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
