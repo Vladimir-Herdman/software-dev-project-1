025cb1fecdd7672321645c3780dd821bd504a748
c-collert1 <158203307+c-collert1@users.noreply.github.com>
2025-09-10 12:19:06 -0400

    fixed platform manager

--- Scripts/PlatformManager.gd
+++ Scripts/PlatformManager.gd
@@ -9,6 +9,24 @@ extends Node
 var lastPlatform: Node2D
 const MAX_PLATFORMS: int = 7
 var platformCount: int = 0;
+var TIMER_TIME = 1
+var timer
+
+func _ready() -> void:
+		timer = TIMER_TIME
+		var initialPlatforms = 0
+		while initialPlatforms < 7:
+			var offset_x = randi_range(-800, 800)  
+			var offset_y = randi_range(-150, -200)
+			
+			var pos: Vector2
+			if lastPlatform:
+				pos = _getPlatformPosition(lastPlatform.position)
+			else:
+				pos = Vector2(600, 800)
+				
+			spawn_platform("basic", pos)
+			initialPlatforms += 1
 
 func spawn_platform(platform_type: String, position: Vector2) -> void:
 	var platform: Node2D
@@ -36,18 +54,34 @@ func spawn_platform(platform_type: String, position: Vector2) -> void:
 	
 
 func _process(_delta: float) -> void:
-	while platformCount < MAX_PLATFORMS:
+	
+	if timer < 0.1:
+		print("Making a platform")
 		var offset_x = randi_range(-800, 800)  
 		var offset_y = randi_range(-150, -200)
 		
 		var pos: Vector2
 		if lastPlatform:
-			pos = lastPlatform.position + Vector2(offset_x, offset_y)
+			pos = _getPlatformPosition(lastPlatform.position)
 		else:
 			pos = Vector2(600, 800)
-
+			
 		spawn_platform("basic", pos)
+		timer = TIMER_TIME
+	else:
+		
+		timer -= _delta
+		print("changin timer " + str(timer))
 
 
 func _on_rat_horde_platform_destroy() -> void:
 	platformCount -= 1
+	
+func _getPlatformPosition(oldPos: Vector2) -> Vector2:
+	var offset_y = randi_range(230, 320)
+	var xPos: float
+	var safe = false
+
+	xPos = randi_range(200,1800)
+
+	return Vector2(xPos, oldPos.y - offset_y)
